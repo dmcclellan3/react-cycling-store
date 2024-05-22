ff97bb92a5ae2ed6178a7a115da236aebf064f34
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-21 20:38:33 -0400

    save

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 
+
 function About() {
   return (
     <div className="p-5">
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,56 @@
 import { Link } from "react-router-dom"
+import axios from 'axios'
+import {useEffect} from 'react' 
+import {useState} from 'react'
+
+const vehicleList = ({ vehicles }) => {
+  return vehicles.list > 0 ? (
+    <div>
+      <h2>Vehicles</h2>
+      {vehicles.map(vehicle => {
+        return (
+          <div key={vehicle.id}>
+           {vehicle.id} - {vehicle.name}
+          </div>
+        )
+      })}
+    </div>
+  ) : (
+    <div>Loading...</div>
+  )
+}
+
+const NewVehicle = ({ getVehicles }) => {
+  const [name, setName] = useState('')
+
+  const createVehicle= () => {
+    axios.post('http://127.0.0.1:8000/vehicles/', {
+      name: name
+    })
+    .then(response => {
+      console.log('RESPONSE: ', response)
+      if(response.status === 200) {
+        setName('')
+        getVehicles()
+      }
+    })
+    .catch(error => console.log('ERROR: ', error))
+  }
+}
+
+  return (
+    <div>
+      <h2>Create A New Vehicle</h2>
+      <input 
+      onChange={e => setName(e.target.value)}
+      placeholder='enter vehicle'
+      value={vehicle}
+      />
+      <button onClick={() => createVehicle()}>
+        Create Vehicle
+      </button>
+    </div>
+  )
 
 const Title = () => {
   return (
@@ -11,8 +63,8 @@ const Title = () => {
 function App() {
   return (
     <div className="p-5">
-      <Link to='/about'>About</Link>
-      <Title />
+      <vehicleList vehicles={vehicles} />
+      
     </div>
   )
 }
